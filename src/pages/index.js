060540b9dca055f204215aa2460da659c5fae80a
odavidu <odaviduu@gmail.com>
2025-02-25 00:14:06 -0500

    Convert header image to .webp format and reduce dimensions to increase loading speed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import Heading from '@theme/Heading';
 import styles from './index.module.css';
-import background from '../assets/background.jpg';
+import background from '../assets/background.webp';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
