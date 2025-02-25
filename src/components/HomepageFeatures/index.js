4d2221143155d558875f76be9caf6cba35200fd5
SunehB <sunehbhatia@gmail.com>
2025-02-25 00:01:13 -0500

    added 3 unique features of our experiment section

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,38 +4,39 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Codebase',
-    Svg: require('@site/static/img/computer-monitor-svgrepo-com.svg').default,
+    title: 'Gamification of Interventions',
+    imgSrc: '/img/OEVLWG0.jpg',
     description: (
       <>
-        
+        Gamify economic interventions for realistic modeling of advertising and purchasing behavior by participants.
       </>
     ),
   },
   {
-    title: 'Documentation',
-    Svg: require('@site/static/img/notepad-with-spring-svgrepo-com.svg').default,
+    title: 'Interactive Experiments',
+    imgSrc: '/img/55868.jpg',
     description: (
       <>
+      Our marketplace is interactive and real-time so participants can play in versus mode, simulating real-life advertising.
       </>
     ),
   },
   {
-    title: 'Explanation',
-    Svg: require('@site/static/img/eco-lightbulb-symbol-svgrepo-com.svg').default,
+    title: 'Replicable Science',
+    imgSrc: '/img/replicablescience.jpg',
     description: (
       <>
-        
+        Re-run experiments with the same settings at the click of a button, making scientific experimentation trivially reproducible.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({imgSrc, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+      <img src={imgSrc} alt={title} className={styles.featureImage} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
@@ -48,14 +49,6 @@ function Feature({Svg, title, description}) {
 export default function HomepageFeatures() {
   return (
     <div>
-      <section className={styles.supportedBySection} data-aos="zoom-in">
-        <h3>Supported By</h3>
-        <div className={styles.logosContainer}>
-          <img src="/img/InstituteMark_DBI_RGB.png" alt="Logo" className={styles.supportedLogo} />
-          <img src="/img/NSF_Logo.png" alt="Logo 2" className={styles.supportedLogo} />
-        </div>
-      </section>
-
       <section className={styles.features}>
         <div className="container">
           <div className="row">
@@ -65,6 +58,15 @@ export default function HomepageFeatures() {
           </div>
         </div>
       </section>
+      <section className={styles.supportedBySection} data-aos="zoom-in">
+        <h3>Supported By</h3>
+        <div className={styles.logosContainer}>
+          <img src="/img/InstituteMark_DBI_RGB.png" alt="Logo" className={styles.supportedLogo} />
+          <img src="/img/NSF_Logo.png" alt="Logo 2" className={styles.supportedLogo} />
+        </div>
+      </section>
+
+      
     </div>
   );
 }
